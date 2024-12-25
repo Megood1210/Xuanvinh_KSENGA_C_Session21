8a8efd3c4de720fbcc6ebd964180392f8e2c89e7
Megood1210 <xuanvinhle06@gmail.com>
2024-12-25 12:21:47 +0700

    Upload bài tập 8

---
+++ Session21 bài 8.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+typedef struct{
+    int id;
+    char name[100];
+    int age;
+}Student;
+int main(){
+    Student students[100];
+    int n=0;
+    FILE *fptr=fopen("students.txt","r");
+    if (!fptr) return 1;
+    while (fscanf(fptr,"%d,%99[^,], %d\n",&students[n].id,students[n].name,&students[n].age)==3){
+        n++;
+        if (n>=100); 
+		break;
+    }
+    fclose(fptr);
+    for (int i=0;i<n;i++){
+        printf("ID: %d\n,Ten: %s\n,Tuoi: %d\n",students[i].id,students[i].name,students[i].age);
+    }
+    return 0;
+}
+
